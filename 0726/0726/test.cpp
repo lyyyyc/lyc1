3f5048f96783e8ed7cf431872aac705fe15ca6dd
李宇晨 <liyuchengong@qq.com>
2019-07-28 16:28:35 +0800

    修改

--- 0726/0726/test.cpp
+++ 0726/0726/test.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<string.h>
 #include<stdio.h>
+#include<malloc.h>
+#include<assert.h>
 using namespace std;
 
 #if 0
@@ -88,6 +91,7 @@ int main()
 #endif
 
 //选择排序
+#if 0
 void Swap(int* left, int* right)
 {
 	int tmp = *left;
@@ -125,6 +129,332 @@ int main()
 	int array[] = { 3,5,2,9,1,7,6,0,8,4 };
 	PrintArray(array, sizeof(array) / sizeof(array[0]));
 	SelectSort(array, sizeof(array) / sizeof(array[0]));
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	return 0;
+}
+#endif
+
+//堆排序
+#if 0
+void HeapSort(int* array, int size)
+{
+
+}
+#endif
+
+//冒泡排序
+#if 0
+void Swap(int* left, int* right)
+{
+	int tmp = *left;
+	*left = *right;
+	*right = tmp;
+}
+void BubbleSort(int* array, int size)
+{
+	int end = size - 1;
+	while (end > 0)
+	{
+		for (int i = end - 1; i > +0; i--)
+		{
+			if (array[end] < array[i])
+			{
+				Swap(&array[end], &array[i]);
+			}
+			end--;
+		}
+	}
+}
+void PrintArray(int* array, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	int array[] = { 3,5,2,9,1,7,6,0,8,4 };
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	BubbleSort(array, sizeof(array) / sizeof(array[0]));
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	return 0;
+}
+#endif
+
+
+#if 0
+void InsertSoft(int* array, int size)
+{
+	for (int i = 1; i < size; ++i)
+	{
+		int key = array[i];//待插入元素
+		int end = i - 1;
+		while (end >= 0 && key < array[end])
+		{
+			array[end + 1] = array[end];
+			end--;
+		}
+		array[end + 1] = key;
+	}
+}
+//快速排序
+void Swap(int* left, int* right)
+{
+	int tmp = *left;
+	*left = *right;
+	*right = tmp;
+}
+//三数取中法
+int GeiMiddleIndex(int* array, int left, int right)
+{
+	int mid = left+((right-left) >> 1);
+	if (array[left] < array[right - 1])
+	{
+		if (array[mid] < array[left])
+		{
+			return left;
+		}
+		else if (array[mid] > array[right - 1])
+		{
+			return right - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	else
+	{
+		if (array[mid] > array[left])
+		{
+			return left;
+		}
+		else if (array[mid] < array[right-1])
+		{
+			return right - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+}
+//hoare版本
+int Partion1(int* array, int left, int right)
+{
+	int begin = left;
+	int end = right - 1;
+	int mid = GeiMiddleIndex(array, left, right);
+	Swap(&array[mid], &array[right - 1]);
+	int key = array[right-1];
+	while (begin<end)
+	{
+		while (begin < end && array[begin] <= key)
+		{
+			begin++;
+		}
+		while (begin < end && array[end] >= key)
+		{
+			end--;
+		}
+		if (begin != end)
+		{
+			Swap(&array[begin], &array[end]);
+		}
+	}
+	if (begin != right - 1)
+	{
+		Swap(&array[begin], &array[right - 1]);
+	}
+	return begin;
+}
+//挖坑法
+int Partion2(int* array, int left, int right)
+{
+	int begin = left;
+	int end = right - 1;
+	int mid = GeiMiddleIndex(array, left, right);
+	Swap(&array[mid], &array[right - 1]);
+	int key = array[right - 1];
+	while (begin < end)
+	{
+		//从前往后找比基准值大的元素，找到后填充end位置的坑
+		while (begin < end && array[begin] <= key)
+		{
+			begin++;
+		}
+		//填充end位置的坑
+		if (begin < end)
+		{
+			array[end] = array[begin];
+			end--;
+		}
+		//从后往前找比基准值小的元素，找到后填充begin位置的坑
+		while (begin < end && array[end] >= key)
+		{
+			end--;
+		}
+		//填充begin位置的坑
+		if (begin < end)
+		{
+			array[begin] = array[end];
+			begin++;
+		}
+	}
+	array[begin] = key;
+	return begin;
+}
+//前后指针
+int Partion3(int* array, int left, int right)
+{
+	int cur = left;
+	int pre = left - 1;
+	int mid = GeiMiddleIndex(array, left, right);
+	Swap(&array[mid], &array[right - 1]);
+	int key = array[right - 1];
+	while (cur<right)
+	{
+		if (array[cur] < array[right - 1]&&++pre!=cur)
+		{
+			Swap(&array[pre], &array[cur]);
+		}
+		cur++;
+	}
+	if (pre < right)
+	{
+		Swap(&array[++pre], &array[right - 1]);
+	}
+	return pre;
+}
+void QuickSort(int* array, int left,int right)
+{
+	if (right - left < 16)
+	{
+		InsertSoft(array, right - left);
+	}
+	else
+	{
+		//按照基准值将区间分割成左右两部分
+		//左侧小于基准值，右侧大于基准值
+		int div = Partion3(array, left, right);
+		QuickSort(array, left, div);
+		QuickSort(array, div+1, right);
+	}
+}
+void PrintArray(int* array, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	int array[] = { 3,4,2,9,1,7,6,0,8,5 };
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	QuickSort(array,0, sizeof(array) / sizeof(array[0]));
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	return 0;
+}
+#endif
+
+//归并排序
+void MergeData(int* array, int left, int mid, int right,int *tmp)
+{
+	int begin1 = left;
+	int end1 = mid;
+	int begin2 = mid;
+	int end2 = right;
+	int index = left;
+	while (begin1 < end1 && begin2 < end2)
+	{
+		if (array[begin1] < array[begin2])
+		{
+			tmp[index++] = array[begin1++];
+		}
+		else
+		{
+			tmp[index++] = array[begin2++];
+		}
+	}
+	while (begin1 < end1)
+	{
+		tmp[index++] = array[begin1++];
+	}
+	while (begin2 < end2)
+	{
+		tmp[index++] = array[begin2++];
+	}
+}
+void _MergeSort(int *array,int left,int right,int *tmp)
+{
+	if (right - left > 1)
+	{
+		int mid = left + ((right - left) >> 1);
+		_MergeSort(array, left, mid,tmp);
+		_MergeSort(array, mid, right,tmp);
+		MergeData(array, left, mid, right,tmp);
+		memcpy(array+left, tmp+left, sizeof(int) * (right - left));
+	}
+}
+void MergeSort(int* array, int size)
+{
+	int* tmp = (int*)malloc(sizeof(int)*size);
+	if (NULL == tmp)
+	{
+		assert(0);
+		return;
+	}
+	_MergeSort(array, 0,size,tmp);
+	free(tmp);
+}
+void MergeSortCir(int* array, int size)//循环归并
+{
+	int* tmp = (int*)malloc(sizeof(int) * size);
+	if (NULL == tmp)
+	{
+		assert(0);
+		return;
+	}
+	int gap = 1;
+	while (gap<size)
+	{
+		for (int i = 0; i < size; i += 2 * gap)
+		{
+			int left = i;
+			int mid = left + gap;
+			int right = mid + gap;
+			if (mid >= size)
+			{
+				mid = size;
+			}
+			if (right >= size)
+			{
+				right = size;
+			}
+			MergeData(array, left, mid, right, tmp);
+		}
+		memcpy(array, tmp, sizeof(int) * size);
+		gap *= 2;
+	}
+	free(tmp);
+}
+void PrintArray(int* array, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	int array[] = { 3,4,2,9,1,7,6,0,8,5 };
+	PrintArray(array, sizeof(array) / sizeof(array[0]));
+	MergeSortCir(array, sizeof(array) / sizeof(array[0]));
 	PrintArray(array, sizeof(array) / sizeof(array[0]));
 	return 0;
 }
--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -698,6 +698,7 @@ int main()
 "()(()()",7
 返回：false*/
 
+#if 0
 class Parenthesis 
 {
 public:    
@@ -727,3 +728,106 @@ public:
 		return true;    
 	} 
 }; 
+#endif
+
+/*标题：两种排序方法 | 时间限制：1秒 | 内存限制：32768K 
+考拉有n个字符串字符串，任意两个字符串长度都是不同的。
+考拉最近学习到有两种字符串的排序方法： 
+1.根据字符串的字典序排序。例如：
+"car" < "carriage" < "cats" < "doggies < "koala"
+2.根据字符串的长度排序。例如：
+"car" < "cats" < "koala" < "doggies" < "carriage"
+考拉想知道自己的这些字符串排列顺序是否满足这两种排序方法，
+考拉要忙着吃树叶，所以需要你来帮忙验 证。 
+输入描述： 
+输入第一行为字符串个数n(n ≤ 100) 接下来的n行,每行一个字符串,
+字符串长度均小于100，均由小写字母组成 
+输出描述： 
+如果这些字符串是根据字典序排列而不是根据长度排列输出"lexicographically",
+如果根据长度排列而不是字典序排列输出"lengths",
+如果两种方式都符合输出"both"，否则输出"none" 
+示例1: 
+输入 
+3 
+a 
+aa 
+bbb 
+输出 
+both
+*/
+
+#if 0
+#include<iostream> 
+#include<vector> 
+#include<string> 
+using namespace std;
+
+int main() 
+{
+	int n; 
+	cin >> n; 
+	vector<string> v; 
+	v.resize(n); 
+	for (auto& str : v)     
+		cin >> str;
+	bool lenSym = true, lexSym = true; 
+	// 这里要注意从i=1开始遍历，前后比较，比较长度 
+	for(size_t i = 1; i < v.size(); ++i) 
+	{     
+		if(v[i-1].size() >= v[i].size())     
+		{         
+			lenSym = false;         
+			break;     
+		} 
+	} 
+	//比较ASCII码 
+	for(size_t i = 1; i < v.size(); ++i) 
+	{     
+		if(v[i-1] >= v[i])     
+		{         
+			lexSym = false;         
+			break;     
+		} 
+	}      
+	if (lenSym&& lexSym)  
+		cout<<"both"<<endl; 
+	else if (!lenSym && lexSym)     
+		cout<<"lexicographically"<<endl; 
+	else if (lenSym && !lexSym)    
+		cout<<"lengths"<<endl; 
+	else if (!lenSym&&!lexSym)    
+		cout<<"none"<<endl;
+	return 0;
+}
+#endif
+
+/*标题：求最小公倍数 | 时间限制：1秒 | 内存限制：32768K 
+正整数A和正整数B的最小公倍数是指能被A和B整除的最小的正整数值，
+设计一个算法，求输入A和B的最小公倍数。
+输入描述： 
+输入两个正整数A和B。
+输出描述： 
+输出A和B的最小公倍数。
+*/
+
+#include<iostream> 
+using namespace std;
+int gcd(int a, int b) 
+{ 
+	int r; 
+	while(r = a%b)
+	{     
+		a = b;     
+		b = r; 
+	} 
+	return b;
+} 
+int main()
+{ 
+	int a, b; 
+	while (cin >> a >> b) 
+	{ 
+		cout << a * b / gcd(a, b) << endl; 
+	} 
+	return 0; 
+}
