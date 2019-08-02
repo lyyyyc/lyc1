13b93ca01308db05b27573c8c85436fb8827f6a2
李宇晨 <liyuchengong@qq.com>
2019-08-02 16:07:53 +0800

    修改

--- 0801/0801/test.cpp
+++ 0801/0801/test.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<memory>
 using namespace std;
 
 #if 0
@@ -162,6 +164,7 @@ int main()
 }
 #endif
 
+#if 0
 template<class T>
 class SmartPtr
 {
@@ -182,5 +185,601 @@ int main()
 	int* p = new int;
 	SmartPtr<int> sp(p);
 	//delete p;
+	return 0;
+}
+#endif
+
+#if 0
+#include<iostream>
+
+using namespace std;
+
+int maxTimes(int n)
+{
+	if (n == 1)
+	{
+		return 0;
+	}
+	if (n <= 3)
+	{
+		return 1;
+	}
+	return 1 + maxTimes((n + 2) / 3);
+}
+int main()
+{
+	int n;
+	while (cin >> n && n)
+	{
+		cout << maxTimes(n) << endl;
+	}
+	return 0;
+}
+#endif
+
+#if 0
+//#include<iostream> 
+//using namespace std;
+class B
+{
+public:
+	B()
+	{
+		cout << "default constructor" << " ";
+	}
+	~B()
+	{
+		cout << "destructed" << " ";
+	}
+	B(int i) :
+		data(i)
+	{
+		cout << "constructed by parameter" << data << " ";
+	}
+private:
+	int data;
+};
+B Play(B b)
+{
+	return b;
+}
+int main(int argc, char* argv[])
+{
+	B temp = Play(5);
+	return 0;
+}
+#endif
+
+#if 0
+namespace bit
+{
+	template<class T>
+	class auto_ptr
+	{
+	public:
+		auto_ptr(T* ptr=nullptr)
+			:_ptr(ptr)
+		{}
+
+		auto_ptr(auto_ptr<T>& ap)
+			:_ptr(ap._ptr)
+		{
+			ap._ptr = nullptr;
+		}
+
+		auto_ptr<T>& operator=(auto_ptr<T>& ap)
+		{
+			if (this != &ap)
+			{
+				if (_ptr)
+				{
+					delete _ptr;
+				}
+				_ptr = ap._ptr;
+				ap._ptr = nullptr;
+			}
+			return *this;
+		}
+
+		~auto_ptr()
+		{
+			if (_ptr)
+			{
+				delete _ptr;
+				_ptr = nullptr;
+			}
+		}
+
+		T& operator*()
+		{
+			return *_ptr;
+		}
+
+		T* operator->()
+		{
+			return _ptr;
+		}
+
+	private:
+		T* _ptr;
+	};
+}
+
+struct A
+{
+	int a;
+	int b;
+	int c;
+};
+
+void TestAuto1()
+{
+	bit::auto_ptr<int> ap1(new int);
+	*ap1 = 10;
+
+	int a = 10;
+	int* pa = &a;
+	A aa;
+	A* paa = &aa;
+	paa->a;
+	
+	bit::auto_ptr<A> ap2(new A);
+	(*ap2).a;
+	ap2->a;
+}
+
+void TestAuto2()
+{
+	bit::auto_ptr<int>ap1(new int);
+	bit::auto_ptr<int>ap2(ap1);
+
+	bit::auto_ptr<int>ap3;
+	ap3 = ap2;
+}
+
+int main()
+{
+	TestAuto2();
+	return 0;
+}
+#endif
+
+
+#if 0
+//资源管理权（对资源释放的权利）的转移
+namespace bit
+{
+	template<class T>
+	class auto_ptr
+	{
+	public:
+		auto_ptr(T* ptr = nullptr)
+			:_ptr(ptr)
+			, _owner(false)
+		{
+			if (_ptr)
+			{
+				_owner = true;
+			}
+		}
+		auto_ptr<T>& operator=(auto_ptr<T>& ap)
+		{
+			if (this != &ap)
+			{
+				if (_ptr)
+				{
+					delete _ptr;
+				}
+				_ptr = ap._ptr;
+				_owner = ap._owner;
+				ap._owner = false;
+				
+			}
+			return *this;
+		}
+		~auto_ptr()
+		{
+			if (_ptr&&_owner)
+			{
+				delete _ptr;
+				_owner = false;
+				_ptr = nullptr;
+			}
+		}
+		T& operator*()
+		{
+			return *_ptr;
+		}
+		T* operator->()
+		{
+			return _ptr;
+		}
+		auto_ptr(const auto_ptr<T>& ap)
+			:_ptr(ap._ptr)
+			, _owner(ap._owner)
+		{
+			ap._owner = false;
+		}
+	private:
+		T* _ptr;
+		mutable bool _owner;//资源的释放者
+	};
+}
+void TestAutoPtr()
+{
+	bit::auto_ptr<int>ap1(new int);
+	bit::auto_ptr<int>ap2(ap1);
+	*ap1 = 10;
+	*ap2 = 20;
+
+	bit::auto_ptr<int>ap3;
+	ap3 = ap2;
+}
+int main()
+{
+	TestAutoPtr();
+	return 0;
+}
+#endif
+
+#if 0
+namespace bit
+{
+	template<class T>
+	class unique_ptr//资源独占，不允许发生拷贝
+	{
+	public:
+		unique_ptr(T* ptr = nullptr)
+			:_ptr(ptr)
+		{}
+		~unique_ptr()
+		{
+			if (_ptr)
+			{
+				delete _ptr;
+				_ptr = nullptr;
+			}
+		}
+		T& operator* ()
+		{
+			return *_ptr;
+		}
+		T* operator->()
+		{
+			return _ptr;
+		}
+		//防止被拷贝
+		//C++98
+	/*private:
+		unique_ptr(const unique_ptr<T>&);
+		unique_ptr<T>& operator=(const unique_ptr<T>& ap);*/
+		
+		//C++11 delete:禁止默认成员函数的生成
+		unique_ptr(const unique_ptr<T>&) = delete;
+		unique_ptr<T>& operator=(const unique_ptr<T>& ap) = delete;
+	private:
+		T* _ptr;
+	};
+}
+class A
+{
+public:
+	A(int a)
+		:_a(a)
+	{}
+	A() = default;//生成默认的构造函数
+private:
+	int _a;
+};
+void TestA()
+{
+	A a1(10);
+	A a2;
+}
+void TestUniquePtr()
+{
+	bit::unique_ptr<int>up1(new int);
+	bit::unique_ptr<int>up2(up1);
+	bit::unique_ptr<int>up3;
+	up3 = up1;
+}
+int main()
+{
+	TestUniquePtr();
+	return 0;
+}
+#endif
+
+#if 0
+#include<memory>
+int main()
+{
+	/*auto_ptr<int>ap1(new int);
+	auto_ptr<int>ap2(ap1);
+	auto_ptr<int>ap3;
+	ap3 = ap2;*/
+	unique_ptr<int>up1(new int);
+	unique_ptr<int>up2(up1);
+	unique_ptr<int>up3;
+	up3 = up2;
+	return 0;
+}
+#endif
+
+#if 0
+namespace bit
+{
+	template<class T>
+	class shared_ptr
+	{
+	public:
+		shared_ptr(T* ptr = nullptr)
+			:_ptr(ptr)
+		{
+			if (_ptr)
+			{
+				_count = 1;
+			}
+		}
+		shared_ptr(shared_ptr<T>& sp)
+			:_ptr(sp._ptr)
+		{
+			_count++;
+		}
+		~shared_ptr()
+		{
+			if (_ptr && 0 == --_count)
+			{
+				delete _ptr;
+				_ptr = nullptr;
+			}
+		}
+	private:
+		T* _ptr;//资源
+		static int _count;
+	};
+	template<class T>
+	int shared_ptr<T>::_count = 0;
+}
+void TestSharedPtr()
+{
+	bit::shared_ptr<int>sp1(new int);
+	bit::shared_ptr<int>sp2(sp1);
+	bit::shared_ptr<int>sp3(new int);
+}
+int main()
+{
+	TestSharedPtr();
+	return 0;
+}
+#endif
+
+#if 0
+namespace bit
+{
+	//只能管理new的资源
+	template<class T>
+	class shared_ptr
+	{
+	public:
+		shared_ptr(T* ptr = nullptr)
+			:_ptr(ptr)
+			, _pCount(nullptr)
+		{
+			if (_ptr)
+			{
+				_pCount = new int(1);
+			}
+		}
+		shared_ptr(shared_ptr<T>& sp)
+			:_ptr(sp._ptr)
+			, _pCount(sp._pCount)
+		{
+			if (_ptr)
+			{
+				++(*_pCount);
+			}
+		}
+		//sp2=sp1
+		//sp2：没有管理资源，与sp1直接共享
+		//sp2：独自管理资源，
+		//sp2：与其他对象共享资源，计数-1
+		shared_ptr<T>& operator=(const shared_ptr<T>& sp)
+		{
+			if (this != &sp)
+			{
+				//让当前资源与管理资源断开联系
+				release();
+				//让当前对象与sp共享资源
+				_ptr = sp._ptr;
+				_pCount = sp._pCount;
+				if (_ptr)
+				{
+					++(*_pCount);
+				}
+			}
+			return *this;
+		}
+		~shared_ptr()
+		{
+			release();
+		}
+		int use_count()
+		{
+			return *_pCount;
+		}
+	private:
+		void release()
+		{
+			if (_ptr && 0 == -- * _pCount)
+			{
+				delete _ptr;
+				_ptr = nullptr;
+				delete _pCount;
+				_pCount = nullptr;
+			}
+		}
+	private:
+		T* _ptr;//资源
+		int* _pCount;//指向计数的空间
+	};
+}
+void TestSharedPtr1()
+{
+	bit::shared_ptr<int>sp1(new int);
+	bit::shared_ptr<int>sp2(sp1);
+	cout << sp1.use_count() << endl;
+	cout << sp2.use_count() << endl;
+	bit::shared_ptr<int>sp3(new int);
+	cout << sp3.use_count() << endl;
+}
+void TestSharedPtr2()
+{
+	bit::shared_ptr<int>sp1(new int);
+	bit::shared_ptr<int>sp2;
+	sp2 = sp1;
+
+	bit::shared_ptr<int>sp3(new int);
+	bit::shared_ptr<int>sp4(new int);
+	sp4 = sp3;
+
+	bit::shared_ptr<int>sp5(new int);
+	bit::shared_ptr<int>sp6(new int);
+	bit::shared_ptr<int>sp7(sp6);
+	sp6 = sp5;
+}
+int main()
+{
+	TestSharedPtr2();
+	return 0;
+}
+#endif
+
+
+//配置删除器
+//new
+template<class T>
+class DFDel
+{
+public:
+	void operator()(T*& p)
+	{
+		if (p)
+		{
+			delete p;
+			p = nullptr;
+		}
+	}
+};
+//malloc
+template<class T>
+class Free
+{
+public:
+	void operator()(T*& p)
+	{
+		if (p)
+		{
+			free(p);
+			p = nullptr;
+		}
+	}
+};
+//文件
+class FClose
+{
+public:
+	void operator()(FILE*& pf)
+	{
+		if (pf)
+		{
+			fclose(pf);
+			pf = nullptr;
+		}
+	}
+};
+namespace bit
+{
+	//根据资源类型去释放
+	//析构资源时，析构方式必须灵活
+	template<class T,class DF=DFDel<T>>
+	class shared_ptr
+	{
+	public:
+		shared_ptr(T* ptr = nullptr)
+			:_ptr(ptr)
+			, _pCount(nullptr)
+		{
+			if (_ptr)
+			{
+				_pCount = new int(1);
+			}
+		}
+		shared_ptr(shared_ptr<T>& sp)
+			:_ptr(sp._ptr)
+			, _pCount(sp._pCount)
+		{
+			if (_ptr)
+			{
+				++(*_pCount);
+			}
+		}	
+		//sp2=sp1
+		//sp2：没有管理资源，与sp1直接共享
+		//sp2：独自管理资源，
+		//sp2：与其他对象共享资源，计数-1
+		shared_ptr<T>& operator=(const shared_ptr<T>& sp)
+		{
+			if (this != &sp)
+			{
+				//让当前资源与管理资源断开联系
+				release();
+				//让当前对象与sp共享资源
+				_ptr = sp._ptr;
+				_pCount = sp._pCount;
+				if (_ptr)
+				{
+					++(*_pCount);
+				}
+			}
+			return *this;
+		}
+		~shared_ptr()
+		{
+			release();
+		}
+		int use_count()
+		{
+			return *_pCount;
+		}
+	private:
+		void release()
+		{
+			if (_ptr && 0 == -- * _pCount)
+			{
+				//delete _ptr;
+				//DF df;
+				//df.operator()(_ptr)
+				DF()(_ptr);
+				_ptr = nullptr;
+				delete _pCount;
+				_pCount = nullptr;
+			}
+		}
+	private:
+		T* _ptr;//资源
+		int* _pCount;//指向计数的空间
+	};
+}
+void TestSharedPtr()
+{
+	bit::shared_ptr<int>sp1(new int);
+	bit::shared_ptr<int>sp2((int*)malloc(sizeof(int)));
+	bit::shared_ptr<FILE>sp3(fopen("1.txt", "w"));
+
+}
+int main()
+{
+	TestSharedPtr();
 	return 0;
 }
