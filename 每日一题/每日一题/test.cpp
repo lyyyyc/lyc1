9322cf256918a0868c3cf1822ce847442c261a89
李宇晨 <liyuchengong@qq.com>
2019-08-02 11:15:46 +0800

    添加

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -1218,6 +1218,7 @@ int main()
 2
 */
 
+#if 0
 #include<iostream>	
 #include<algorithm>
 
@@ -1244,5 +1245,88 @@ int main()
 		}
 		cout << maxcount << endl;
 	}
+	return 0;
+}
+#endif
+
+/*标题：二进制插入 | 时间限制：3秒 | 内存限制：32768K | 语言限制：[Python, C++, C#, Java] 
+有两个32位整数n和m，请编写算法,
+将m的二进制数位插入到n的二进制的第j到第i位,
+其中二进制的位数从低位数到高位且以0开始。
+给定两个数int n和int m，同时给定int j和int i，意义如题所述，
+请返回操作后的数，保证n的第j到第i位均为零，
+且m的二进制位数小于等于i-j+1。
+测试样例：
+1024，19，2，6
+返回：
+1100
+*/
+
+#if 0
+#include<iostream>
+using namespace std;
+class BinInsert 
+{ 
+public:
+	int binInsert(int n, int m, int i, int j)
+	{ 
+		m <<= j; 
+		return n | m; 
+	} 
+};
+int main()
+{
+	BinInsert b;
+	cout<<b.binInsert(1024, 19, 6, 2)<<endl;
+	return 0;
+}
+#endif
+
+/*标题：查找组成一个偶数最接近的两个素数 | 时间限制：1秒 | 内存限制：32768K 
+任意一个偶数（大于2）都可以由2个素数组成，
+组成偶数的2个素数有很多种情况，
+本题目要求输出组成指定偶数的两个素数差值最小的素数对 
+输入描述： 
+输入一个偶数
+输出描述： 
+输出两个素数
+示例1: 
+输入 
+20
+输出
+7 
+13*/
+
+#include<iostream> 
+#include<algorithm> 
+using namespace std; 
+//判断素数 
+bool isPrime(int num) 
+{  
+	int tmp = sqrt(num);
+	//只遍历到开方出，不能被开方左边的数整除，则一定不能被开方右边的数整除 
+	for (int i = 2; i <= tmp; i++)   
+	{    
+		if (num %i == 0)        
+			return false;
+	}
+	return true; 
+}
+int main()
+{
+	int num;   
+	int half; 
+	int i;    
+	while (cin >> num) 
+	 {
+		half = num / 2; 
+		//从中间向两边找    
+		for (i = half; i > 0; i--)     
+		{         
+			if (isPrime(i) && isPrime(num - i))      
+				break;      
+		}
+		cout << i << endl << num - i << endl;
+	 }   
 	return 0;
 }
