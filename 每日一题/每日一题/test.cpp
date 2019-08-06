afa8393da52da693ebec4400c223d614b3bb923d
李宇晨 <liyuchengong@qq.com>
2019-08-06 17:11:53 +0800

    add

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -1438,7 +1438,7 @@ int main()
 5
 */
 
-
+#if 0
 #include <iostream> 
 #include <vector> 
 #include <algorithm> 
@@ -1494,3 +1494,255 @@ int main()
 	cout << Jump(n, m) << endl;
 	return 0; 
 }
+#endif
+
+/*标题：计算日期到天数转换 | 时间限制：1秒 | 内存限制：32768K 
+根据输入的日期，计算是这一年的第几天。。
+详细描述：
+输入某年某月某日，判断这一天是这一年的第几天？。
+ 
+接口设计及说明：
+ /***************************************************************************** 
+Description   : 数据转换 
+Input Param   : 
+year 输入年份                
+Month 输入月份                
+Day 输入天                     
+Output Param  : 
+Return Value  : 
+成功返回0，失败返回-1（如：数据错误） *****************************************************************************
+public static int iConverDateToDay(int year, int month, int day)
+{
+	/* 在这里实现功能，将结果填入输入数组中*
+	return 0;
+}
+/*****************************************************************************
+Description   :
+Input Param   :
+Output Param  :
+Return Value  : 
+成功:返回outDay输出计算后的第几天;                                          
+失败:返回-1 *****************************************************************************
+public static int getOutDay()
+{
+	return 0;
+}
+输入描述：
+输入三行，分别是年，月，日
+输出描述：
+成功:返回outDay输出计算后的第几天;                                           
+失败:返回 - 1
+	示例1 :
+	输入
+	2012
+	12
+	31
+	输出
+	366
+	*/
+
+#if 0
+#include <iostream> 
+using namespace std; 
+int main() 
+{
+	int array[12] = {31,59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+	int year; 
+	int month;
+	int day;
+	int sum = 0;
+	while(cin >> year >> month >> day) 
+	{ 
+		sum = 0; 
+		sum += array[month - 2]; 
+		sum += day;
+		if(month > 2) 
+		{ 
+			if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) 
+			{     
+				sum += 1; 
+			} 
+		} 
+		cout << sum <<endl;
+	}   
+	return 0;
+} 
+#endif
+
+/*标题：幸运的袋子 | 时间限制：1秒 | 内存限制：32768K 
+一个袋子里面有n个球，每个球上面都有一个号码(拥有相同号码的球是无区别的)。
+如果一个袋子是幸运的当且仅当所有球的号码的和大于所有球的号码的积。
+例如：
+如果袋子里面的球的号码是{1, 1, 2, 3}，这个袋子就是幸运的，
+因为1 + 1 + 2 + 3 > 1 * 1 * 2 * 3
+你可以适当从袋子里移除一些球(可以移除0个,但是别移除完)，
+要使移除后的袋子是幸运的。
+现在让你编程计算一下你可以获得的多少种不同的幸运的袋子。 
+输入描述： 
+第一行输入一个正整数n(n ≤ 1000) 
+第二行为n个数正整数xi(xi ≤ 1000) 
+输出描述： 
+输出可以产生的幸运的袋子数 
+示例1: 
+输入 
+3 
+1 1 1 
+输出 
+2
+*/
+
+#if 0
+#include<iostream> 
+#include<algorithm>
+using namespace std; 
+/*getLuckyPacket:从当前位置开始搜索符合要求的组合，
+一直搜索到最后一个位置结束  
+x[]: 袋子中的所有球   
+n: 球的总数   
+pos: 当前搜索的位置   
+sum: 到目前位置的累加和  
+multi: 到目前位置的累积值 */ 
+int getLuckyPacket(int x[], int n, int pos, int sum, int multi) 
+{    
+	int count = 0;   
+	//循环，搜索以位置i开始所有可能的组合 
+	for (int i = pos; i<n; i++)  
+	{      
+		sum += x[i];    
+		multi *= x[i];     
+		if (sum > multi) 
+		{          
+			//找到符合要求的组合，加1，继续累加后续的值，看是否有符合要求的集合   
+			count += 1 + getLuckyPacket(x, n, i + 1, sum, multi);     
+		}      
+		else if (x[i] == 1)     
+		{         
+			//如果不符合要求，且当前元素值为1，则继续向后搜索         
+			count += getLuckyPacket(x, n, i + 1, sum, multi);     
+		}
+		else
+		{         
+			//如果sum大于multi,则后面就没有符合要求的组合了   
+			break;     
+		}        
+		//要搜索下一个位置之前，首先恢复sum和multi  
+		sum -= x[i];     
+		multi /= x[i];     
+		//数字相同的球，没有什么区别，都只能算一个组合，所以直接跳过
+		while (i < n - 1 && x[i] == x[i + 1])    
+		{        
+			i++;     
+		}   
+	}   
+	return count; 
+} 
+int main() 
+{  
+	int n; 
+	while (cin >> n)   
+	{    
+		int x[n];
+		for (int i = 0; i < n; i++)  
+		{        
+			cin >> x[i];    
+		}    
+		sort(x, x + n);    
+		//从第一个位置开始搜索    
+		cout << getLuckyPacket(x, n, 0, 0, 1) << endl; 
+	}  
+	return 0; 
+}
+#endif
+
+/*标题：查找输入整数二进制中1的个数 | 时间限制：1秒 | 内存限制：32768K 
+请实现如下接口
+public
+	static int findNumberOf1(int num)
+	{
+		 /* 请实现 *   
+		return 0;
+	}
+	譬如：
+		输入5 ，5的二进制为101，输出2
+		输入描述：
+		输入一个整数
+		输出描述：
+		计算整数二进制中1的个数
+		示例1 :
+	输入
+		5
+		输出
+		2
+*/
+
+#if 0
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int count = 0;
+		while (n)
+		{
+			if ((n & 1) == 1)
+			{
+				count += 1;
+			}
+			n >>= 1;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
+#endif
+
+/*标题：手套 | 时间限制：3秒 | 内存限制：32768K | 语言限制：[Python, C++, C#, Java] 
+在地下室里放着n种颜色的手套，手套分左右手，
+但是每种颜色的左右手手套个数不一定相同。
+A先生现在要出门，所以他要去地下室选手套。
+但是昏暗的灯光让他无法分辨手套的颜色，只能分辨出左右手。
+所以他会多拿一些手套，然后选出一双颜色相同的左右手手套。
+现在的问题是，他至少要拿多少只手套(左手加右手)， 
+才能保证一定能选出一双颜色相同的手套。
+给定颜色种数n(1≤n≤13),同时给定两个长度为n的数组left,right,
+分别代表每种颜色左右手手套的数量。
+数据保证左右的手套总数均不超过26，且一定存在至少一种合法方案。
+测试样例：
+4,[0,7,1,6],[1,5,0,6]
+返回：
+10
+(解释：可以左手手套取2只，右手手套取8只)
+*/
+
+
+
+
+class Gloves {
+public:  
+	int findMinimum(int n, vector<int> left, vector<int> right)
+	{
+		int left_sum = 0, left_min = INT_MAX;     
+		int right_sum = 0, right_min = INT_MAX;  
+		int sum = 0;     
+		//遍历每一种颜色的左右手套序列     
+		for(int i=0;i<n;i++)
+		{            
+			//对于有0存在的颜色手套，累加       
+			if(left[i]*right[i]==0)              
+				sum += left[i] + right[i];         
+			//对于左右手都有的颜色手套，执行累加-最小值+1  
+			//找到最小值和总数           
+			else{           
+				left_sum += left[i];          
+				right_sum += right[i];        
+				left_min = min(left_min, left[i]);       
+				right_min = min(right_min, right[i]);  
+			}     
+		}       
+		//结果为有左右都有数量的手套序列的结果+有0存在的手套数+最后再加一肯定就能保证了 
+		return sum + min(left_sum-left_min+1, right_sum-right_min+1) + 1;   
+	}
+};
