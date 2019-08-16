ff783ebd5422ec1a34a028614fe287608e6a0722
李宇晨 <liyuchengong@qq.com>
2019-08-16 11:02:08 +0800

    add

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -2479,6 +2479,7 @@ UUUU
 7
 */
 
+#if 0
 #include <iostream>
 #include <string> 
 using namespace std; 
@@ -2551,3 +2552,109 @@ int main()
 	}   
 	return 0; 
 } 
+#endif
+
+/*标题：小易的升级之路 | 时间限制：1秒 | 内存限制：32768K 
+小易经常沉迷于网络游戏.有一次,他在玩一个打怪升级的游戏,
+他的角色的初始能力值为 a.在接下来的一段时间内,
+他将会依次遇见n个怪物,每个怪物的防御力为b1,b2,b3...bn. 
+如果遇到的怪物防御力bi小于等于小易的当前能力值c,
+那么他就能轻松打败怪物,并且使得自己的能力值增加bi;如果bi大于c,
+那他也能打败怪物,但他的能力值只能增加bi与c的最大公约数.
+那么问题来了,在一系列的锻炼后,小易的最终能力值为多少?
+输入描述： 
+对于每组数据,第一行是两个整数n(1≤n<100000)表示怪物的数量和a表示小易的初始能力值. 
+第二行n个整数,b1,b2...bn(1≤bi≤n)表示每个怪物的防御力 
+输出描述： 
+对于每组数据,输出一行.每行仅包含一个整数,表示小易的最终能力值 
+示例1: 
+输入
+3 50 
+50 105 200 
+5 20 
+30 20 15 40 100
+输出 
+110 
+205*/
+
+#if 0
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int GCD(int a, int b)
+{
+	int c;
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	return b;
+}
+int main()
+{
+	int n, power;
+	vector<int> num;
+	while (cin >> n >> power)
+	{
+		num.resize(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> num[i];
+		}
+		for (int j = 0; j < n; ++j)
+		{
+			if (num[j] < power)
+			{
+				power += num[j];
+			}
+			else
+			{
+				power += GCD(power, num[j]);
+			}
+		}
+		cout << power << endl;
+	}
+	return 0;
+}
+#endif
+
+/*标题：找出字符串中第一个只出现一次的字符(题面已经更新) | 时间限制：1秒 | 内存限制：32768K 
+找出字符串中第一个只出现一次的字符
+输入描述： 
+输入一个非空字符串
+输出描述： 
+输出第一个只出现一次的字符，如果不存在输出-1
+示例1: 
+输入 
+asdfasdfo
+输出 
+o
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	while (getline(cin, str))
+	{
+		unsigned int i;
+		for (i = 0; i < str.size(); i++)
+		{
+			if (str.find(str[i]) == str.rfind(str[i]))
+			{
+				cout << str[i] << endl;
+				break;
+			}
+		}
+		if (i == str.size())
+			cout << "-1" << endl;;
+	}
+	return 0;
+}
