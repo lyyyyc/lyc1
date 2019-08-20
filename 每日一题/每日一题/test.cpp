883d761d8d89f475e12e477d61898a5a9b833f44
李宇晨 <liyuchengong@qq.com>
2019-08-20 10:24:49 +0800

    add

--- 每日一题/每日一题/test.cpp
+++ 每日一题/每日一题/test.cpp
@@ -3075,6 +3075,7 @@ int main()
 3
 */
 
+#if 0
 #include <iostream> 
 #include <string> 
 int numRoot(int num)
@@ -3108,3 +3109,67 @@ int main()
 	}
 	return 0;
 }
+#endif
+
+/*标题：变态跳台阶 | 时间限制：1秒 | 内存限制：32768K | 语言限制：[Javascript_V8, Python, C++, Javascript, Php, C#, Java] 
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+求该青蛙跳上一个n级的台阶总共有多少种跳法。*/
+
+#if 0
+class Solution 
+{
+public:
+	int jumpFloorII(int number)
+	{
+		//2^(n-1) == 1 << (n-1)   
+		return 1<<(number-1);
+	}
+};
+#endif
+
+/*标题：快到碗里来 | 时间限制：1秒 | 内存限制：32768K 
+小喵们很喜欢把自己装进容器里的（例如碗），
+但是要是碗的周长比喵的身长还短，它们就进不去了。
+现在告诉你它们的身长，和碗的半径，请判断一下能否到碗里去。 
+输入描述： 
+输入有多组数据。
+每组数据包含两个整数n (1≤n≤2^128) 和r (1≤r≤2^128)，
+分别代表喵的身长和碗的半径。
+圆周率使用3.14。 
+输出描述： 
+对应每一组数据，如果喵能装进碗里就输出“Yes”；否则输出“No”。 
+示例1: 
+输入 
+6 1 
+7 1 
+9876543210 1234567890 
+输出 
+Yes 
+No 
+No
+*/
+
+#if 0
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double n, r;
+	while (cin >> n >> r)
+	{
+		double rad = 2 * 3.14 * r;
+		if (n > rad)
+		{
+			cout << "No";
+		}
+		else
+		{
+			cout << "Yes";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+#endif
